4b06692c443670bdb175cc171e068f338f10d597
yoon <sooin0105@gmail.com>
2024-11-23 15:27:48 +0900

    최종

--- recowith/src/components/NavigationBar/NavigationBar.jsx
+++ recowith/src/components/NavigationBar/NavigationBar.jsx
@@ -1,21 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import './NavigationBar.css';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./NavigationBar.css";
 
 const NavigationBar = () => {
   const navigate = useNavigate();
-  const location = useLocation(); 
-  const [activePage, setActivePage] = useState(location.pathname); 
+  const location = useLocation();
+  const [activePage, setActivePage] = useState(location.pathname);
 
   useEffect(() => {
     setActivePage(location.pathname);
   }, [location.pathname]);
 
   const navItems = [
-    { path: '/community', icon: 'main-com.png', activeIcon: 'main-com-col.png' },
-    { path: '/write', icon: 'main-write.png', activeIcon: 'main-write-col.png' },
-    { path: '/',  icon: 'main-home.png', activeIcon: 'main-home-col.png' },
-    { path: '/diary-review', icon: 'main-chart.png', activeIcon: 'main-chart-col.png' },
+    {
+      path: "/community",
+      icon: "main-com.png",
+      activeIcon: "main-com-col.png",
+    },
+    {
+      path: "/write",
+      icon: "main-write.png",
+      activeIcon: "main-write-col.png",
+    },
+    { path: "/main", icon: "main-home.png", activeIcon: "main-home-col.png" },
+    {
+      path: "/diary-review",
+      icon: "main-chart.png",
+      activeIcon: "main-chart-col.png",
+    },
   ];
 
   return (
@@ -23,8 +35,8 @@ const NavigationBar = () => {
       {navItems.map((item) => (
         <div
           key={item.path}
-          onClick={() => navigate(item.path)} 
-          className={`nav-item ${activePage === item.path ? 'active' : ''}`} 
+          onClick={() => navigate(item.path)}
+          className={`nav-item ${activePage === item.path ? "active" : ""}`}
         >
           <img
             src={
@@ -41,4 +53,4 @@ const NavigationBar = () => {
   );
 };
 
-export default NavigationBar;  
+export default NavigationBar;
